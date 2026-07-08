7a343112ccf8e0d719cc836a4cb5cecab287553e
Akrm Al-Hakimi <alk9fh@bosch.com>
2026-07-08 15:48:53 -0400

    examples: replace custom NetworkManager D-Bus hotspot setup with nmrs 3.4 APIs
    Use nmrs 3.4.0's builder activation API for hotspot creation instead of
    local zbus proxy definitions and manual settings conversion. Also switch Wi-Fi
    device discovery to the typed WifiDevice API and drop direct zbus/zvariant
    dev-dependencies from the example.
    
    Signed-off-by: Akrm Al-Hakimi <akrm@cachebag.sh>

--- examples/main/nmrs_extensions.rs
+++ examples/main/nmrs_extensions.rs
@@ -1,103 +1,35 @@
-//! Hotspot workaround code
+//! Hotspot setup helpers.
 
-use std::collections::HashMap;
-use zbus::{Connection, proxy};
-use zvariant::{OwnedObjectPath, OwnedValue};
+use nmrs::{
+    NetworkManager,
+    builders::{WifiConnectionBuilder, WifiMode},
+};
 
-/// Type alias matching what NM's D-Bus API expects
-type NmSettings = HashMap<String, HashMap<String, OwnedValue>>;
-
-#[proxy(
-    interface = "org.freedesktop.NetworkManager",
-    default_service = "org.freedesktop.NetworkManager",
-    default_path = "/org/freedesktop/NetworkManager"
-)]
-trait NetworkManagerProxy {
-    // Returns list of device object paths
-    fn get_devices(&self) -> zbus::Result<Vec<OwnedObjectPath>>;
-}
-
-#[proxy(
-    interface = "org.freedesktop.NetworkManager.Settings",
-    default_service = "org.freedesktop.NetworkManager",
-    default_path = "/org/freedesktop/NetworkManager/Settings"
-)]
-trait NmSettingsProxy {
-    fn add_and_activate_connection(
-        &self,
-        connection: &NmSettings,
-        device: &OwnedObjectPath,
-        specific_object: &OwnedObjectPath,
-    ) -> zbus::Result<(OwnedObjectPath, OwnedObjectPath)>;
-}
-
-// The correct interface for AddAndActivateConnection is actually on NM itself:
-#[proxy(
-    interface = "org.freedesktop.NetworkManager",
-    default_service = "org.freedesktop.NetworkManager",
-    default_path = "/org/freedesktop/NetworkManager"
-)]
-trait NmProxy {
-    fn add_and_activate_connection(
-        &self,
-        connection: &NmSettings,
-        device: &OwnedObjectPath,
-        specific_object: &OwnedObjectPath,
-    ) -> zbus::Result<(OwnedObjectPath, OwnedObjectPath)>;
-}
-
-#[proxy(
-    interface = "org.freedesktop.NetworkManager.Device",
-    default_service = "org.freedesktop.NetworkManager"
-)]
-trait NmDeviceProxy {
-    #[zbus(property)]
-    fn interface(&self) -> zbus::Result<String>;
-}
-
-/// Convert the output of nmrs to a usable form that is not borrowed
-fn to_owned_settings(
-    input: HashMap<&str, HashMap<&str, zvariant::Value<'_>>>,
-) -> HashMap<String, HashMap<String, OwnedValue>> {
-    input
-        .into_iter()
-        .map(|(section, props)| {
-            let owned_props = props
-                .into_iter()
-                .map(|(k, v)| (k.to_string(), v.try_to_owned().unwrap()))
-                .collect();
-            (section.to_string(), owned_props)
-        })
-        .collect()
-}
-
-/// Start a hotspot connection
-pub async fn start_hotspot(ssid: String, psk: String, wifi_dev_path: &str) -> Result<(), String> {
-    let hotspot = nmrs::builders::WifiConnectionBuilder::new(&ssid)
-        .wpa_psk(&psk)
-        .autoconnect(false)
-        .mode(nmrs::builders::WifiMode::Ap)
-        .build();
-    let hr = build_hotspot(wifi_dev_path, hotspot).await;
-    log::info!("The result of making a hotspot is {hr:#?}");
-    Ok(())
-}
-
-/// construct an access point or hotspot
-async fn build_hotspot(
-    wifi_hw: &str,
-    settings: HashMap<&str, HashMap<&str, zvariant::Value<'_>>>,
+/// Start a hotspot connection on the provided Wi-Fi interface.
+pub async fn start_hotspot(
+    nm: &NetworkManager,
+    ssid: &str,
+    psk: &str,
+    wifi_interface: &str,
 ) -> Result<(), String> {
-    let settings = to_owned_settings(settings);
-    let dbus = Connection::system().await.map_err(|e| e.to_string())?;
-    let wifi_device = OwnedObjectPath::try_from(wifi_hw).map_err(|e| e.to_string())?;
-    let any: OwnedObjectPath = OwnedObjectPath::try_from("/").unwrap();
-    let nm = NmProxyProxy::new(&dbus).await.map_err(|e| e.to_string())?;
-    let (conn_path, active_conn_path) = nm
-        .add_and_activate_connection(&settings, &wifi_device, &any)
+    let settings = WifiConnectionBuilder::new(ssid)
+        .wpa_psk(psk)
+        .autoconnect(false)
+        .mode(WifiMode::Ap)
+        .ipv4_shared()
+        .ipv6_ignore()
+        .build();
+
+    let (profile_path, active_path) = nm
+        .add_and_activate_connection(settings, Some(wifi_interface), None)
         .await
         .map_err(|e| e.to_string())?;
-    println!("Connection object path:        {}", conn_path);
-    println!("Active connection object path: {}", active_conn_path);
+
+    log::info!(
+        "Started hotspot profile {} as active connection {}",
+        profile_path,
+        active_path
+    );
+
     Ok(())
 }
